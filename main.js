33733b602f2c872b8d3aa53112ead21d84a4052d
Alessandro David <alessandrodp2222@gmail.com>
2023-04-24 19:29:17 -0500

    Arreglo e implemento una funcion a mi codigo

--- main.js
+++ main.js
@@ -3,15 +3,20 @@
 let n = parseInt(prompt("¿Cuántas notas desea introducir?"));
 let suma = 0;
 
-for (let i = 1; i <= n; i++) {
-    let numero = parseInt(prompt("Introduzca la nota N°" + i + ":"));
-    while (numero < 0 || numero > 20){
-        alert("INTRODUCIR UN NUMERO ENTRE EL 0 Y EL 20")
-        numero = parseInt(prompt("Introduzca la nota N°" + i + ":"));
+introduceNumero();
+
+function introduceNumero() {
+    for (let i = 1; i <= n; i++) {
+        let numero = parseInt(prompt("Introduzca la nota N°" + i + ":"));
+        while (numero < 0 || numero > 20){
+            alert("INTRODUCIR UN NUMERO ENTRE EL 0 Y EL 20")
+            numero = parseInt(prompt("Introduzca la nota N°" + i + ":"));
+        }
+        suma += parseInt(numero);
+        promedio = parseInt(suma)/ n;
     }
-    suma += parseInt(numero);
-    promedio = parseInt(suma)/ n;
 }
+
 
 alert("El promedio de las notas es: " + promedio );
 
